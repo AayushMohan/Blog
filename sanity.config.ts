757eaf81563d11d60a6449e6d5882e27814967c1
AayushMohan <aayushmohan1702@gmail.com>
2022-12-28 23:53:20 +0530

    Updated Sanity Config

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,12 +3,17 @@ import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 import { schemaTypes } from './schemas';
 
-export default defineConfig({
-  name: 'default',
-  title: 'blog',
 
-  projectId: 'p5qrd26i',
-  dataset: 'production',
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
+export default defineConfig({
+  basePath:'/studio',
+  name: 'BLOG_Content_Studio',
+  title: 'BLOG Content Studio',
+
+  projectId,
+  dataset,
 
   plugins: [deskTool(), visionTool()],
 
