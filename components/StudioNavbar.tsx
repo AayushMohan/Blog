442fb2efc4663f7ad3e7db74a1dd46db3d888b6e
AayushMohan <aayushmohan1702@gmail.com>
2022-12-30 21:55:12 +0530

    Completed StudioNavbar Component

--- components/StudioNavbar.tsx
+++ components/StudioNavbar.tsx
@@ -1,8 +1,18 @@
 import Link from "next/link";
 import { ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
 
-const StudioNavbar = () => {
-  return <div>StudioNavbar</div>;
+const StudioNavbar = (props: any) => {
+  return (
+    <div>
+      <div>
+        <Link href="/" className="text-[#F7AB0A] flex items-center">
+          <ArrowUturnLeftIcon className="h-6 w-6 text-[#F7AB0A] mr-2" />
+          Go To Website
+        </Link>
+      </div>
+      <>{props.renderDefault(props)}</>
+    </div>
+  );
 };
 
 export default StudioNavbar;
