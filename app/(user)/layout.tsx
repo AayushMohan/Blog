44beca407bd9c8c295fae550f0f81145e1c7649d
AayushMohan <aayushmohan1702@gmail.com>
2023-01-04 23:16:04 +0530

    Added Header Component

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,3 +1,4 @@
+import Header from "../../components/Header";
 import "../styles/globals.css";
 
 export default function RootLayout({
@@ -8,6 +9,7 @@ export default function RootLayout({
   return (
     <html>
       {/* Header */}
+      <Header />
       {/* Banner */}
       <body>{children}</body>
     </html>
