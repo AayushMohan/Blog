73c74e82fdf779d905d9d9e35332eecaba4b6da0
AayushMohan <aayushmohan1702@gmail.com>
2023-01-08 21:54:12 +0530

    Updated Pages

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,5 +1,6 @@
 import { previewData } from "next/headers";
 import { groq } from "next-sanity";
+import { client } from "../../lib/sanity.client";
 
 const query = groq`
   *[_type == 'post']{
@@ -9,16 +10,15 @@ const query = groq`
   } | order(_createdAt desc)
 `;
 
-const page = () => {
+export default async function HomePage() {
   if (previewData()) {
     return <div>Preview Mode</div>;
   }
 
+  const posts = await client.fetch(query);
   return (
     <div>
       <h1>Not in Preview Mode</h1>
     </div>
   );
-};
-
-export default page;
+}
