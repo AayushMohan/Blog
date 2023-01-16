135ba16ce0eb2b4ba6924217b391dd44150248f1
AayushMohan <aayushmohan1702@gmail.com>
2023-01-16 21:17:57 +0530

    Added urlFor and BlogList

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import urlFor from "../lib/urlFor";
+import post from "../schemas/post";
 import { Post } from "../typings";
 
 type Props = {
@@ -8,6 +11,22 @@ const BlogList = ({ posts }: Props) => {
   return (
     <div>
       <hr className="border-[#F7AB0A] mb-10" />
+
+      <div>
+        {/* Posts */}
+        {posts.map((post) => (
+          <div key={post._id} className="flex flex-col group cursor-pointer">
+            <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
+              <Image
+                className="object-cover object-left lg:object-center"
+                src={urlFor(post.mainImage).url()}
+                alt={post.author.name}
+                fill
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
